a1097bb9839506f72087aec4a12f729a351dfeac
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-09-03 12:35:06 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <body>
         <Main />
