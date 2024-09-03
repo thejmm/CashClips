2e1f056e49385ab6f02a2a5bdee44c7bde8925a7
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-09-03 12:35:23 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
       disableTransitionOnChange
     >
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <Component {...pageProps} />
       <Toaster richColors />
